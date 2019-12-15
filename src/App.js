6a2ca0731ae8dfae22e3528cd9d1d85660328cb7
Gowtham Behara <gothsiddu@gmail.com>
2019-12-15 15:50:04 +0530

    Define func to compute percentage from num of days passed and render

--- src/App.js
+++ src/App.js
@@ -8,9 +8,22 @@ class App extends Component {
     super();
 
     this.state = {
-      percentage: 0,
+      percentage: NaN,
       day: new Date().toLocaleString()
     };
+
+    this.updatePercentage = this.updatePercentage.bind(this);
+  }
+
+  updatePercentage() {
+    const dayOfYear = moment(this.state.day).dayOfYear();
+    const daysInYear = moment().isLeapYear ? 366 : 355;
+
+    this.setState({ percentage: parseFloat(dayOfYear / daysInYear * 100).toFixed(2) });
+  }
+
+  componentDidMount() {
+    setTimeout(this.updatePercentage, 1000);
   }
 
   render() {
@@ -20,6 +33,9 @@ class App extends Component {
           <h4>Year In Progress</h4>
           <div className="today">
             {moment(this.state.day).format('DD MMM YYYY')}
+          </div>
+          <div className="progress">
+            {this.state.percentage ? this.state.percentage : ""}
           </div>
         </header>
       </div>
